dcc3265740dc8dadd3d7d19875bbfe1397f7c6db
Kimi <119054671+Rakhimi@users.noreply.github.com>
2026-06-03 17:51:16 +0800

    logout

---
+++ app/api/logout/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const res = NextResponse.redirect(
+    new URL("/", process.env.NEXT_PUBLIC_BASE_URL)
+  );
+
+  res.cookies.delete("n8s_session");
+
+  return res;
+}
